1ecd1a8e497dfdff5bea50057fcffcacf99e3e2d
Blake <fosterbhilleshiem@gmail.com>
2022-08-01 08:19:13 -0600

    created mock data and rendered

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,28 @@
 import "../styles/App.scss";
 import ProfileItem from "./ProfileItem";
+import mockDataCall from "../mock-data/mockDataCall";
+
+// console.log(mockDataCall);
+
+function TestRender() {
+  return (
+    <div className="row-wrapper">
+      {Array(5)
+        .fill(true)
+        .map((_, i) => (
+          <ProfileItem key={i} id={i} />
+        ))}
+      {/* <ProfileItem id={props.id} />; */}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header>
         <div className="header-left-wrapper">
-          <div className="logo">Descriptive Logo</div>
+          <div className="logo">Peeps n Profiles</div>
         </div>
         <div className="header-center-wrapper">
           <button>Nav Button</button>
@@ -19,13 +35,11 @@ function App() {
         </div>
       </header>
       <div className="middle-container">
-        <div className="row-wrapper">
-          <ProfileItem />
-          <ProfileItem />
-          <ProfileItem />
-          <ProfileItem />
-          <ProfileItem />
-        </div>
+        {/* <div className="row-wrapper"> */}
+
+        {/* <ProfileItem id={2} /> */}
+        <TestRender />
+        {/* </div> */}
       </div>
       <footer>
         <div className="footer-center-wrapper">
---
+++ src/mock-data/mockDataCall.js
@@ -0,0 +1,62 @@
+let mockDataCall = {
+  message: "ok",
+  results: [
+    {
+      id: 1,
+      name: "Blake",
+      description: "A line of text",
+      img: "https://images.unsplash.com/photo-1539418561314-565804e349c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    },
+
+    {
+      id: 2,
+      name: "Jacob",
+      description: "Another line of text",
+      img: "https://images.unsplash.com/photo-1437622368342-7a3d73a34c8f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1856&q=80",
+    },
+
+    {
+      id: 3,
+      name: "Dakotah",
+      description: "Some line of text",
+      img: "https://images.unsplash.com/photo-1517849845537-4d257902454a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    },
+
+    {
+      id: 4,
+      name: "Arlen",
+      description: "A different line of text",
+      img: "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    },
+
+    {
+      id: 5,
+      name: "Aimee",
+      description: "The other line of text",
+      img: "https://images.unsplash.com/photo-1519750783826-e2420f4d687f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    },
+
+    {
+      id: 6,
+      name: "Tanya",
+      description: "Just line of text",
+      img: "https://images.unsplash.com/photo-1628148061873-13b9340f763b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80",
+    },
+
+    {
+      id: 7,
+      name: "Mike",
+      description: "Arbitrary line of text",
+      img: "https://images.unsplash.com/photo-1511447333015-45b65e60f6d5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGxpZ2h0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+    },
+
+    {
+      id: 8,
+      name: "Matt",
+      description: "Random line of text",
+      img: "https://images.unsplash.com/photo-1585110396000-c9ffd4e4b308?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    },
+  ],
+};
+
+export default mockDataCall;
